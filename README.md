02cbfa0a24b8844d0b74844c067f475544f3d28e
Yun <yuniya.pa98@gmail.com>
2022-07-04 11:37:51 -0400

    updated grid in JS, working on boarder and key buttons

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Maze
-Game for project1 
+
+Game for project1
+Trello link with wireframe in it :
+https://trello.com/b/NWIfagnk/maze
+
+# materials:
+
+JavaScript Tutorial 32 - Keyboard events in JavaScript:
+https://www.youtube.com/watch?v=VBVOXTgXX2U&t=409s
+How to Move an Object with Arrow Keys in Javascript:
+https://www.youtube.com/watch?v=NiG2TnZiFL0
--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const lives = document.querySelector('.lives')
 const live1 = document.querySelector('#Heart1')
 const live2 = document.querySelector('#Heart2')
 const live3 = document.querySelector('#Heart3')
+
+//Board and element with in
 const board = document.querySelector('#board')
-const gridSq = document.querySelectorAll('.grid')
-//action sq
-const liveSq = document.querySelectorAll('#lv')
-//const finSq = document.querySelector('#fin')
 
 //windows
 
@@ -25,70 +23,94 @@ const againButton = document.querySelector('#again')
 const character = document.querySelector('#character')
 let moveCharct = 40
 //game speed
-let gameSpeed = 7
-//velocity for moving character
-let xVelocity = 0
-let yVelocity = 0
 
-let gameBoard = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
-]
+//velocity for moving character
+
 //////////////////////////
 //Functions
+//Creating grid
 
-//game loop
-const game = () => {
-  console.log('game')
-  setTimeout(game, 1000 / gameSpeed)
-}
-//game()
-//character speed
-// const characterSpeed = () =>{
-//     character.style.top = character.style.top +
-// }
-
+// let exitCombination = [boardGrid[2[0]], boardGrid[2[1]], boardGrid[2[2]], boardGrid[3[2]], boardGrid[4[2]], boardGrid[4[3]], boardGrid[4[4]], boardGrid[3[4]], boardGrid[3[5]], boardGrid[3[6]]]
 //game logic
 const win = () => {
-  if (
-    character.style.top ===  ||
-    character.style.left === 
-  ) {
+  if (character.style.top === winCombination || character.style.left) {
     console.log('win')
 
     winWindow.style.display = 'block'
-    //window.removeEventListener('keyup', (e))
-  }else if(){
-    //for try again
-    console.log('another try')
-    failWindow.style.display = "block"
-    live3.style.display = 'none'
-    if(live3.style.display = 'none' ){
-    live2.style.display = 'none'
-    }else if(live3.style.display = 'none' && live2.style.display = 'none'){
-        //loose
-        live1.style.display = 'none'
-        //button to start from the beggining with 3 hearts
-    }
-  }else{
-    console.log('error!')
+    // window.removeEventListener('keyup', (e))
+    //   }else if(gridSq){
+    //     //for try again
+    //     console.log('another try')
+    //     failWindow.style.display = "block"
+    //     live3.style.display = 'none'
+    //     if(live3.style.display = 'none'){
+    //     live2.style.display = 'none'
+    //     }else if(live3.style.display = 'none' || live2.style.display = 'none' ){
+    //         //loose
+    //         live1.style.display = 'none'
+    //         //button to start from the beggining with 3 hearts
+    //     }
+    //   }else{
+    //     console.log('error!')
   }
 }
 
 ////////////////////////////
 //EventListeners
+//creating board
+window.addEventListener('load', () => {
+  let boardGrid = [
+    [1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19, 20, 21],
+    [22, 23, 24, 25, 26, 27, 28],
+    [29, 30, 31, 32, 33, 34, 35],
+    [36, 37, 38, 39, 40, 41, 42]
+  ]
+  let maze = [
+    boardGrid[[2][0]],
+    boardGrid[[2][1]],
+    boardGrid[[2][2]],
+    boardGrid[[3][2]],
+    boardGrid[[4][2]],
+    boardGrid[[4][3]],
+    boardGrid[[4][4]],
+    boardGrid[[3][4]],
+    boardGrid[[3][5]],
+    boardGrid[[3][6]]
+  ]
+  for (let i of boardGrid) {
+    for (let j of i) {
+      let square = document.createElement('div')
+      square.innerHTML = j
+      square.className = 'square'
+      board.appendChild(square)
+      board.appendChild(character)
+      for (let k = 0; k < maze.length; k++) {
+        if (boardGrid[[i][j]] === maze[k]) {
+          square.id = 'maze'
+        }
+      }
+      //   let square = document.querySelectorAll('.square')
+      //   maze[k] === (square.class = 'maze')
+    }
+  }
+})
+
 //event listener for buttons to move character
 //start position
 window.addEventListener('load', () => {
   character.style.left = -20 + 'px'
-  character.style.top = 160 + 'px'
+  character.style.top = 180 + 'px'
   character.style.position = 'abosolute'
 })
 //button key
-window.addEventListener('keyup', (e) => {
+
+window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'ArrowLeft':
       character.style.left = parseInt(character.style.left) - moveCharct + 'px'
+
       break
     case 'ArrowRight':
       character.style.left = parseInt(character.style.left) + moveCharct + 'px'
@@ -100,6 +122,7 @@ window.addEventListener('keyup', (e) => {
       character.style.top = parseInt(character.style.top) + moveCharct + 'px'
       break
   }
+  //win()
 })
 
 //again button if lives still there keep on the lvl else start from lvl 1 lives back
